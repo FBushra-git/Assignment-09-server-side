3892bdcf22c305b0862a59d29f77c708ccbecb52
Bushra <mafiaqueen734@gmail.com>
2026-06-09 23:29:04 +0600

    Middleware function created

--- index.js
+++ index.js
@@ -28,7 +28,24 @@ const JWKS = createRemoteJWKSet(
 );
 
 // verify token
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Foridden" });
+  }
+};
 
 async function run() {
   // await client.connect();
